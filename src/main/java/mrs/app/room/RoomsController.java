080d5aad084436d4d31a3b1d045230c4956ace76
yuki-and <yuki.andoooo@gmail.com>
2019-12-11 16:17:07 +0900

    会議室一覧表示機能の実装

---
+++ src/main/java/mrs/app/room/RoomsController.java
@@ -0,0 +1,30 @@
+package mrs.app.room;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import mrs.domain.model.ReservableRoom;
+import mrs.domain.service.room.RoomService;
+
+@Controller
+@RequestMapping("/rooms")
+public class RoomsController {
+
+	@Autowired
+	RoomService roomService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	String listRooms(Model model) {
+		LocalDate today = LocalDate.now();
+		List<ReservableRoom> rooms = roomService.findByReservableRooms(today);
+		model.addAttribute("date", today);
+		model.addAttribute("rooms", rooms);
+		return "room/listRooms";
+	}
+}
