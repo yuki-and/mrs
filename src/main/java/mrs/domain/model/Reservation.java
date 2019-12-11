28cbbdcebdd6a358716d817a597c1b306c80cd05
yuki-and <yuki.andoooo@gmail.com>
2019-12-11 11:41:31 +0900

    JPAエンティティ作成

---
+++ src/main/java/mrs/domain/model/Reservation.java
@@ -0,0 +1,73 @@
+package mrs.domain.model;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Reservation implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer reservationId;
+
+	private LocalTime startTime;
+
+	private LocalTime endTime;
+
+	@ManyToOne
+	@JoinColumns({ @JoinColumn(name = "reservation_date"), @JoinColumn(name = "room_id") })
+	private ReservableRoom reservationRoom;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Integer getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(Integer reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public ReservableRoom getReservationRoom() {
+		return reservationRoom;
+	}
+
+	public void setReservationRoom(ReservableRoom reservationRoom) {
+		this.reservationRoom = reservationRoom;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
---
+++ src/main/java/mrs/domain/model/User.java
@@ -0,0 +1,63 @@
+package mrs.domain.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usr")
+public class User implements Serializable{
+
+	@Id
+	private String userId;
+	private String password;
+	private String firsrName;
+	private String lastName;
+	
+	@Enumerated(EnumType.STRING)
+	private RoleName roleName;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirsrName() {
+		return firsrName;
+	}
+
+	public void setFirsrName(String firsrName) {
+		this.firsrName = firsrName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public RoleName getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(RoleName roleName) {
+		this.roleName = roleName;
+	}
+}
